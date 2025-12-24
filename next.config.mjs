a511edf84578a2d1c6ef15da7a354e7408d7b3b5
alaminislam34 <mi3548514@gmail.com>
2025-12-25 04:30:04 +0600

    added some issue solution |

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // unoptimized true optional, but safer for external dev URLs
     unoptimized: true,
+
+    // Optional: external URLs you want to allow in production
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cupid-ai-phi.vercel.app", // Replace with your actual domain
+        pathname: "/media/**",
+      },
+    ],
   },
 };
 
