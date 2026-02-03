2dccb8d2aae73922284bc656891b8bbbb34959a0
alaminislam34 <mi3548514@gmail.com>
2026-02-04 05:03:05 +0600

    added

--- src/api/apiEntpoint.js
+++ src/api/apiEntpoint.js
@@ -1,21 +1,16 @@
-// src/api/apiEndpoints.js
 
-// Profile and Auth
 export const PROFILE_DETAILS = "/api/profile/";
 export const PROFILE_UPDATE = "/api/profile/update/";
 export const TOKEN_REFRESH = "/api/token/refresh/";
 export const CREATE_BOT = "/api/bots/";
 export const LOGOUT = "/api/logout/";
 
-// Password Reset
 export const PASSWORD_RESET_REQUEST = "/api/password-reset/request/";
 export const PASSWORD_RESET_VERIFY = "/api/password-reset/verify/";
 
-// otp request
 export const OTP_REQUEST = "/api/login/otp/request/";
 export const OTP_VERIFY = "/api/login/otp/verify/";
 
-// Admin Metrics
 export const ADMIN_TOTAL_USERS = "/api/admin/total-users/";
 export const ADMIN_TOTAL_BOTS = "/api/admin/total-bots/";
 export const ALL_BOTS_DATA = "/api/all-data/";
